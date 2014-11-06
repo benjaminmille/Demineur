c62ab44cbd08ba5ce6429cc3fbaba0a463facc11
Benjamin <benjamin.mille@hotmail.fr>
2014-11-06 14:47:12 +0100

    Lancement du programme

--- Demineur/src/demineur/Demineur.java
+++ Demineur/src/demineur/Demineur.java
@@ -1,10 +1,12 @@
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package demineur;
-
+import java.util.Scanner;
 /**
  *
  * @author Gartok
@@ -16,6 +18,18 @@ public class Demineur {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenue dans le demineur, choisissez votre niveau,"
+                + " tapez D (Débutant) ou E (Expert):");
+        String str = sc.nextLine();
+        if (str == "D" || str == "d") {
+           System.out.println("Vous avez choisi le mode débutant."); 
+        } else if (str == "E" || str == "e"){
+           System.out.println("Vous avez choisi le mode expert."); 
+        } else {
+            System.out.println("Veuillez entrer une lettre valide."); 
+        }
+        System.out.println("Votre highscore est:"); 
     }
     
 }
